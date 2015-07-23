1300f83e50afe24d92556aabc286c621dca29c29
Evan Summers <evans@saratoga.co.za>
2015-07-23 14:30:29 +0200

    update

---
+++ Dates.js
@@ -0,0 +1,20 @@
+// Copyright (c) 2015, Evan Summers (twitter.com/evanxsummers)
+// ISC license, see http://github.com/evanx/redexutil/LICENSE
+
+import assert from 'assert';
+
+import Loggers from './Loggers';
+
+const logger = Loggers.create(module.filename, 'info');
+
+const that = {
+
+   formatShortISO(value) {
+      assert(value);
+      assert(value.constructor.name === 'Date');
+      return value.toISOString().substring(0, 16);
+   }
+
+}
+
+module.exports = that;
