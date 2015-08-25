0f97349ae8fab60921ff3c36d00a4660a210b44f
Evan Summers <evans@saratoga.co.za>
2015-08-25 10:37:57 +0200

    update

--- Collections.js
+++ Collections.js
@@ -5,16 +5,25 @@ import Loggers from './Loggers';
 
 const logger = Loggers.create(module.filename, 'info');
 
-const that = {
-   map(iterable, fn) {
-      //logger.debug('map', typeof iterable);
-      let array = [];
-      for (let item of iterable) {
-         //logger.debug('map item', typeof item, item, fn(item));
-         array.push(fn(item));
-      }
-      return array;
+export function map(iterable, fn) {
+   //logger.debug('map', typeof iterable);
+   let array = [];
+   for (let item of iterable) {
+      //logger.debug('map item', typeof item, item, fn(item));
+      array.push(fn(item));
    }
-};
+   return array;
+}
 
-module.exports = that;
+export async function mapAsync(array, fn) {
+   return await* array.map(async (item) => {
+      try {
+         let result = await fn();
+         logger.info('mapAsync', result);
+         return result;
+      } catch (err) {
+         logger.warn('mapAsync', err);
+         throw err;
+      }
+   });
+}
