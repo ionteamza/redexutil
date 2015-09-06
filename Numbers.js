cb369f74834d8702b5c2e72d3dbf3faa4c57bdef
Evan Summers <evans@saratoga.co.za>
2015-09-06 16:51:26 +0200

    update

--- Numbers.js
+++ Numbers.js
@@ -1,4 +1,16 @@
 
+
+export function parseInt(value, defaultValue) {
+   if (value) {
+      try {
+         return global.parseInt(value);
+      } catch (err) {
+         logger.debug('parseInt', value, defaultValue);
+      }
+   }
+   return defaultValue;
+}
+
 export function isInteger(value) {
    return value && !isNaN(value) && /^[0-9]+$/.test(value.toString());
 }
