8fc9ee14e36cf4494c71f1f1369d0688474f8daf
Evan Summers <evans@saratoga.co.za>
2015-09-14 23:45:43 +0200

    request timeout

--- Requests.js
+++ Requests.js
@@ -2,66 +2,73 @@
 // ISC license, see http://github.com/evanx/redexutil/LICENSE
 
 import fs from 'fs';
-import request from 'request';
+import _request from 'request';
 import lodash from 'lodash';
 
 import Loggers from './Loggers';
 
-const logger = Loggers.create(module.filename, 'info');
+const logger = Loggers.create(module.filename);
 
-module.exports = {
-   requestJson(url) {
-      return module.exports.request({url: url, json: true});
-   },
-   request(options) {
-      logger.debug('request', options);
-      if (lodash.isObject(options)) {
-         if (options.lastModified) {
-            options.headers = {'If-Modified-Since': options.lastModified};
-         }
-      }
-      return new Promise((resolve, reject) => {
-         request(options, (err, response, content) => {
-            logger.debug('response', options.url, err || response.statusCode);
-            if (err) {
-               reject(err);
-            } else if (response.statusCode !== 200) {
-               reject({statusCode: response.statusCode});
-            } else {
-               resolve(content);
-            }
-         });
-      });
-   },
-   response(options) {
-      logger.debug('request', options);
-      return new Promise((resolve, reject) => {
-         request(options, (err, response, content) => {
-            logger.debug('response', options.url, err || response.statusCode);
-            if (err) {
-               reject(err);
-            } else {
-               resolve([response, content]);
-            }
-         });
-      });
-   },
-   head(options) {
-      if (typeof options === 'string') {
-         options = {url: options};
-      }
-      assert(options.url, 'url');
-      options.method = 'HEAD';
-      logger.debug('head', options);
-      return new Promise((resolve, reject) => {
-         request(options, (err, response) => {
-            logger.debug('response', options.url, err || response.statusCode);
-            if (err) {
-               reject(err);
-            } else {
-               resolve(response);
-            }
-         });
-      });
+export function requestJson(options) {
+   if (typeof options === 'string') {
+      options = {url: options};
    }
-};
+   logger.debug('requestJson', options);
+   assert(options.url, 'url');
+   options.json = true;
+   return request(options);
+}
+
+export function request(options) {
+   logger.debug('request', options);
+   if (lodash.isObject(options)) {
+      if (options.lastModified) {
+         options.headers = {'If-Modified-Since': options.lastModified};
+      }
+   }
+   return new Promise((resolve, reject) => {
+      _request(options, (err, response, content) => {
+         logger.debug('response', options.url, err || response.statusCode);
+         if (err) {
+            reject(err);
+         } else if (response.statusCode !== 200) {
+            reject({statusCode: response.statusCode});
+         } else {
+            resolve(content);
+         }
+      });
+   });
+}
+
+export function response(options) {
+   logger.debug('request', options);
+   return new Promise((resolve, reject) => {
+      _request(options, (err, response, content) => {
+         logger.debug('response', options.url, err || response.statusCode);
+         if (err) {
+            reject(err);
+         } else {
+            resolve([response, content]);
+         }
+      });
+   });
+}
+
+export function head(options) {
+   if (typeof options === 'string') {
+      options = {url: options};
+   }
+   assert(options.url, 'url');
+   options.method = 'HEAD';
+   logger.debug('head', options);
+   return new Promise((resolve, reject) => {
+      request(options, (err, response) => {
+         logger.debug('response', options.url, err || response.statusCode);
+         if (err) {
+            reject(err);
+         } else {
+            resolve(response);
+         }
+      });
+   });
+}
