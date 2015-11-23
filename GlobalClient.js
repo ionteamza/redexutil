2eeef6af3d57381175c630d15d57c3f2b922bacb
Evan Summers <evans@saratoga.co.za>
2015-11-23 14:12:09 +0200

    GlobalClient.assign(global)

--- GlobalClient.js
+++ GlobalClient.js
@@ -3,15 +3,15 @@
 
 // Warning: this automatically loads a number of modules in global
 
-export function assign() {
-   global.Arguments = require('./Arguments');
-   global.Arrays = require('./Arrays');
-   global.Asserts = require('./Asserts');
-   global.Dates = require('./Dates');
-   global.Maps = require('./Maps');
-   global.Millis = require('./Millis');
-   global.Numbers = require('./Numbers');
-   global.Objects = require('./Objects');
-   global.Seconds = require('./Seconds');
-   global.Strings = require('./Strings');
+export function assign(g) {
+   g.Arguments = require('./Arguments');
+   g.Arrays = require('./Arrays');
+   g.Asserts = require('./Asserts');
+   g.Dates = require('./Dates');
+   g.Maps = require('./Maps');
+   g.Millis = require('./Millis');
+   g.Numbers = require('./Numbers');
+   g.Objects = require('./Objects');
+   g.Seconds = require('./Seconds');
+   g.Strings = require('./Strings');
 }
