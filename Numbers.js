df0d31c557c6424bebe0f4873369441cad7384ed
Evan Summers <evan.summers@gmail.com>
2015-11-25 19:51:30 +0200

    Numbers logger to __filename for client side

--- Numbers.js
+++ Numbers.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2015, Evan Summers (twitter.com/evanxsummers)
 // ISC license, see http://github.com/evanx/redexutil/LICENSE
 
-const logger = Loggers.create(module.filename, 'info');
+const logger = Loggers.create(__filename, 'info');
 
 export function parseInt(value, defaultValue) {
    if (value) {
