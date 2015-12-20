a04e4254de039fc7a2bca5e590085a1fe01c3d4b
Evan Summers <evan.summers@gmail.com>
2015-12-21 00:18:30 +0200

    clone with exclude props

--- Objects.js
+++ Objects.js
@@ -1,10 +1,24 @@
 // Copyright (c) 2015, Evan Summers (twitter.com/evanxsummers)
 // ISC license, see http://github.com/evanx/redexutil/LICENSE
 
+import lodash from 'lodash';
+
 const logger = Loggers.create(__filename, 'info');
 
 export function values(object) {
    return Object.keys(object).map(key => object[key]);
+}
+
+function createPropPair(key, value) {
+   let pair = {};
+   pair[key] = value;
+   return pair;
+}
+
+export function excludeProps(object, regex) {
+   let props = lodash.compact(Object.keys(object).filter(key => !regex.test(key))
+   .map(key => createPropPair(key, object[key])));
+   return Object.assign({}, ...props);
 }
 
 export function props(object) {
