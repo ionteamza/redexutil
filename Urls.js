74c344bd1b649601558987bdffcc24f5bbd59f16
Evan Summers <evan.summers@gmail.com>
2015-12-27 18:40:12 +0200

    urls

--- Urls.js
+++ Urls.js
@@ -1,9 +1,32 @@
 
+const urlRegex = /https?:\/\/([^\/]+)(\/\S+)$/;
+
 export function pathname(url) {
-   let match = url.match(/https?:\/\/([^\/]+)(\/\S+)$/);
+   const match = url.match(urlRegex);
    if (match) {
       return match[2].replace(/\?.*$/, '');
    } else {
-      return url;
+      logger.error('pathname', url);
+   }
+}
+
+export function hostname(url) {
+   let match = url.match(/https?:\/\/([^\/]+)(\/\S+)$/);
+   if (match) {
+      return match[1];
+   } else {
+      logger.error('hostname', url);
+   }
+}
+
+export function parse(url) {
+   let match = url.match(/https?:\/\/([^\/]+)(\/\S+)$/);
+   if (match) {
+      return {
+         hostname: match[1],
+         pathname: match[2].replace(/\?.*$/, '')
+      };
+   } else {
+      logger.error('parse', url);
    }
 }
