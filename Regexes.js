e3def47bff9260001d21d4114a5646d256403955
Evan Summers <evan.summers@gmail.com>
2016-02-29 17:05:10 +0200

    regexes

---
+++ Regexes.js
@@ -0,0 +1,11 @@
+// Copyright (c) 2015, Evan Summers (twitter.com/evanxsummers)
+// ISC license, see http://github.com/evanx/redexutil/LICENSE
+
+const logger = Loggers.create(__filename, 'info');
+
+export function extract(regex, string) {
+   let match = string.match(regex);
+   if (match && match.length > 1) {
+      return match[1];
+   }
+}
